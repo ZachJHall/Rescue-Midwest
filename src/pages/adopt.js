a7907fe20ef61f7d1e430aa10e083f36d490d33c
ZachJHall <zachjohnhall@gmail.com>
2020-01-07 16:12:42 -0600

    Added adoption page content

--- src/pages/adopt.js
+++ src/pages/adopt.js
@@ -2,13 +2,25 @@ import React from "react"
 import Layout from "../components/layout"
 
 
-const AdoptPage = () => (
-<Layout>
+const AdoptPage = () => {
+  return (
+    <Layout>
 
-  <h1>Adopt Page</h1>
+      <div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScl_XqV34f1XKDs28-ycoL0LCHYjZeyNCEG8KG1L8CZdmoTBQ/viewform?embedded=true"
+          width="640"
+          height="1308"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
 
-</Layout>
-
-)
+    </Layout>
+  )
+}
 
 export default AdoptPage
