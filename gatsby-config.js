a2cc4fd875a48822cb8120b2461de6a69a4d9ef8
ZachJHall <zachjohnhall@gmail.com>
2020-01-07 15:11:06 -0600

    Edited gatsby config and config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-}
+ module.exports = {
+   plugins: [`gatsby-plugin-netlify-cms`],
+ }
