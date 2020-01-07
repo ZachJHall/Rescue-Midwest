cf169f142197062dd4f8760f23afba2185d1fcef
ZachJHall <zachjohnhall@gmail.com>
2020-01-07 16:06:27 -0600

    Added donate and contact page content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,22 @@ import React from "react"
 import Layout from "../components/layout"
 
 
-const ContactPage = () => (
-  <Layout>
+const ContactPage = () => {
+  return (
+    <Layout>
 
-    <h1>Contact Page</h1>
+    <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSd1bEWBDQiSdgi0T8V_r5lGermHNPHXycc3nLaUg31Ok02aXg/viewform?embedded=true"
+        width="640"
+        height="1070"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
 
-  </Layout>
-)
-
+    </Layout>
+  )
+}
 export default ContactPage
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -4,8 +4,57 @@ import Layout from "../components/layout"
 
 const DonatePage = () => (
   <Layout>
-
-    <h1>Donate Page</h1>
+    <div>
+      <h1>Donate</h1>
+      <div>
+        <div className="center">
+          <p>
+            Chihuahua Rescue Midwest understands that there are many people who
+            do not have the time or resources to foster but want to help
+            organizations such as ours. We accept donations that go to the
+            complete benefit of the organization. Our staff is completely
+            volunteer. If you would like to make a donation of any amount,
+            please feel free to click on the "PayPal" button below. It will take
+            you to PayPals website where you can make a donation.
+          </p>
+          <br />
+          <p>
+            We appreciate any assistance that you can offer. And please know
+            that your donation will be utilized towards rescuing animals that
+            would otherwise be left in harm's way without the continued support
+            from people like you.
+          </p>
+          <form
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_top"
+          >
+            <input type="hidden" name="cmd" value="_donations" />
+            <input
+              type="hidden"
+              name="business"
+              value="chihuahuarescuemidwest@gmail.com"
+            />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input
+              type="image"
+              src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+              border="0"
+              name="submit"
+              title="PayPal - The safer, easier way to pay online!"
+              alt="Donate with PayPal button"
+            />
+            <img
+              alt=""
+              border="0"
+              src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+              width="1"
+              height="1"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
 
   </Layout>
 )
