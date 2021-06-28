817d6ba5744cb8baaf3cfd212a7ae6bf4eba0205
zachjhall <zachjohnhall@gmail.com>
2021-06-27 23:28:21 -0500

    Cleanup footer component

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -4,28 +4,7 @@ import "../CSS/footer.css"
 
 export const Footer = () => {
   return (
-    <footer class="footer-basic-centered">
-      {/* <p class="footer-company-motto">
-        Website Designed by{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          id="carterName"
-          href="https://carterwagner.me"
-        >
-          Carter Wagner
-        </a>{" "}
-        and{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          id="zachsName"
-          href="https://www.zachhall.me/"
-        >
-          Zach Hall
-        </a>
-      </p> */}
-    </footer>
+    <footer class="footer-basic-centered" />
   )
 }
 
